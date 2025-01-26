1fed5926400d272dfa80d3441c5b15ff61dcb103
Xiao Jiang <bjjx1999@live.com>
2025-01-26 14:30:15 -0800

    Main level

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Graphics;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public new Camera camera;
+    public BubbleRenderManager bubbleRenderManager;
+    public MeshRenderer refImageRenderer;
+    
+    private Material refImageMaterialInstance;
+    
+    public void SetRefImage(Texture2D texture)
+    {
+        refImageMaterialInstance.SetTexture("_BaseMap", texture);
+    }
+
+    private void Start()
+    {
+        camera.orthographicSize = bubbleRenderManager.transform.localScale.y / 2 / camera.aspect;
+        refImageMaterialInstance = refImageRenderer.material;
+    }
+}
