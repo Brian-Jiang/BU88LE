012f4a8560990e2e81af775aa00a581d9fb75e8b
MIUNOo <104364364+MIUNOo@users.noreply.github.com>
2025-01-26 15:59:37 -0800

    UI Result

---
+++ Assets/Scripts/ShowSimilarity.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+
+public class ShowSimilarity : MonoBehaviour
+{
+
+    public GameObject winUI;
+    public GameObject loseUI;
+    public float threshold;
+
+    private TextMeshPro similarityText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        similarityText = GetComponent<TextMeshPro>();
+    }
+
+
+    bool CalculateSimilarity(float similarity)
+    {
+        similarityText.text = "Similarity: " + similarity.ToString("F2") + " %";
+        if (similarity > threshold)
+        {
+            winUI.SetActive(true);
+            loseUI.SetActive(false);
+            return true;
+        }
+        else
+        {
+            winUI.SetActive(false);
+            loseUI.SetActive(true);
+            return false;
+        }
+    }
+
+    
+}
